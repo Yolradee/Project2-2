100815e5ebe5b4ab9520afa8b398f46029eb0782
SasivimolC <talek.k@hotmail.com>
2015-04-26 03:22:50 +0700

    projectttttt

--- app/src/main/java/com/example/talek/project2/rank.java
+++ app/src/main/java/com/example/talek/project2/rank.java
@@ -1,282 +1,210 @@
 package com.example.talek.project2;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+        import android.content.Intent;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.os.AsyncTask;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.ListView;
+        import android.widget.SimpleAdapter;
+        import android.widget.SimpleCursorAdapter;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Handler;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+        import java.net.URLConnection;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.Map;
+        import java.util.logging.Handler;
 
-public class rank extends ActionBarActivity {
-
-<<<<<<< HEAD
-    ArrayList<HashMap<String, String>> data;
-    MovieDBHelper helper;
-=======
-    ArrayList<Map<String, String>> data;
->>>>>>> origin/master
-    SimpleAdapter adapter;
-    Handler h;
-    Handler handler;
-    String F,T,Ta;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rank);
-<<<<<<< HEAD
-
-        data = new ArrayList<HashMap<String, String>>();
+/**
+ * Created by talek on 4/26/15 AD.
+ */
+public class rank extends ActionBarActivity{
 
 
-        adapter= new SimpleAdapter(this, data, R.layout.col,
-                new String[] {"Fast7", "Thor", "Taken"},
-                new int[] {R.id.nm1, R.id.nm2, R.id.nm3});
-
-        ListView l = (ListView)findViewById(R.id.listView);
-        l.setAdapter(adapter);
-        LoadNumberTask task = new LoadNumberTask();
-        task.execute();
- }
-
-    public void addClicked(View v){
-
-        Intent i = new Intent(this, insertName.class);
-        startActivity(i);
-    }
-=======
->>>>>>> origin/master
-
-        LoadNumberTask l = new LoadNumberTask();
-
-<<<<<<< HEAD
-    public void showClicked(View v) {
-
-        Intent i = new Intent(this, showdata.class);
-        startActivity(i);
-=======
-        l.execute();
-
->>>>>>> origin/master
-    }
+        ArrayList<HashMap<String, String>> data;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rank, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        SimpleAdapter adapter;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_rank);
+
+            data = new ArrayList<HashMap<String, String>>();
+            adapter= new SimpleAdapter(this, data, R.layout.col,
+                    new String[] {"Fast7", "Thor", "Taken"},
+                    new int[] {R.id.nm1, R.id.nm2, R.id.nm3});
+
+            ListView l = (ListView)findViewById(R.id.listView);
+            l.setAdapter(adapter);
+            LoadNumberTask task = new LoadNumberTask();
+            task.execute();
+        }
+
+        public void addClicked(View v){
+
+            Intent i = new Intent(this, insertName.class);
+            startActivity(i);
+        }
+
+
+        public void showClicked(View v) {
+
+            Intent i = new Intent(this, showdata.class);
+            startActivity(i);
+
+   }
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_rank, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-<<<<<<< HEAD
-   class LoadNumberTask extends AsyncTask<String, Void, Boolean> {
-
-       @Override
-       protected Boolean doInBackground(String... params) {
-           BufferedReader reader;
-           StringBuilder buffer = new StringBuilder();
-           String line;
-
-           int fastL;
-           int thorL;
-           int takenL;
-
-
-           try {
-               URL u = new URL("http://ict.siit.tu.ac.th/~u5522773787/its333/fetch.php");
-               HttpURLConnection h = (HttpURLConnection) u.openConnection();
-               h.setDoInput(true);
-               h.connect();
-
-               int response = h.getResponseCode();
-               if (response == 200) {
-                   reader = new BufferedReader(new InputStreamReader(h.getInputStream()));
-                   while ((line = reader.readLine()) != null) {
-                       buffer.append(line);
-                   }
-
-//                   Log.e("LoadNumberTask", buffer.toString());
-
-                   //Parsing JSON and displaying messages
-                   JSONObject json = new JSONObject(buffer.toString());
-                   JSONArray jmsg = json.getJSONArray("msg");
-
-
-                       JSONObject jmessage = jmsg.getJSONObject(0);
-
-                       fastL = jmessage.getInt("Fast7");
-                       thorL = jmessage.getInt("Thor");
-                       takenL = jmessage.getInt("Taken");
-
-
-                       HashMap<String, String> item;
-                       item = new HashMap<String, String>();
-
-                         if(fastL>=thorL && thorL >=takenL) {
-                             item.put("Fast7", String.format("Fast7  " + "%d",fastL));
-                             item.put("Thor", String.format("Thor  " + "%d",thorL));
-                             item.put("Taken", String.format("Taken  " + "%d",takenL));
-                             data.add(0, item);
-                         }
-                          if(fastL>=thorL && thorL <=takenL) {
-                             item.put("Fast7", String.format("Fast7  "+ "%d",fastL));
-                             item.put("Thor", String.format("Thor  "+ "%d",takenL));
-                             item.put("Taken", String.format("Taken  " + "%d",thorL));
-                             data.add(0, item);
-                   }
-                         if(fastL<=thorL && fastL >=takenL) {
-                             item.put("Fast7", String.format("Thor  "+ "%d",thorL));
-                             item.put("Thor", String.format("Fast7  " + "%d",fastL));
-                             item.put("Taken", String.format("Taken  " +"%d",takenL));
-                             data.add(0, item);
-                   }
-                        if(fastL<=thorL && fastL <=takenL) {
-                            item.put("Fast7", String.format("Thor  "+ "%d",thorL));
-                            item.put("Thor", String.format("Taken  " +"%d",takenL));
-                            item.put("Taken", String.format("Fast7  "+ "%d",fastL));
-                            data.add(0, item);
-                   }
-                        if(fastL>=thorL && fastL <=takenL) {
-                            item.put("Fast7", String.format("Taken  " +"%d",takenL));
-                            item.put("Thor", String.format("Fast7  " + "%d",fastL));
-                            item.put("Taken", String.format("Thor  "+ "%d",thorL));
-                            data.add(0, item);
-                   }
-                        if(fastL<=thorL && fastL <=takenL) {
-                            item.put("Fast7", String.format("Taken  " + "%d",takenL));
-                            item.put("Thor", String.format("Thor  " + "%d",thorL));
-                            item.put("Taken", String.format("Fast7  " +"%d",fastL));
-                            data.add(0, item);
-                   }
-
-
-               }
-
-
-               return true;
-
-
-           } catch (MalformedURLException e) {
-               Log.e("LoadNumberTask", "Invalid URL");
-           } catch (IOException e) {
-               Log.e("LoadNumberTask", "I/O Exception");
-           } catch (JSONException e) {
-               Log.e("LoadNumberTask", "Invalid JSON");
-           }
-           return false;
-       }
-
-           protected void onPostExecute(Boolean result) {
-               if (result) {
-                   Log.e("LoadNumberTask", "Successfully Load");
-               } else {
-                   Log.e("LoadNumberTask", "Unable Load");
-
-               }
-           }
-       }
-}
-=======
-    class LoadNumberTask extends AsyncTask<String, Void, Boolean> {
-
         @Override
-        protected Boolean doInBackground(String... params) {
-            BufferedReader reader;
-            StringBuilder buffer = new StringBuilder();
-            String line;
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-            try {
-                URL u = new URL("http://ict.siit.tu.ac.th/~u5522773787/its333/fetch.php");
-                HttpURLConnection h = (HttpURLConnection) u.openConnection();
-                h.setDoInput(true);
-                h.connect();
-
-                int response = h.getResponseCode();
-                if (response == 200) {
-                    reader = new BufferedReader(new InputStreamReader(h.getInputStream()));
-                    while ((line = reader.readLine()) != null) {
-                        buffer.append(line);
-                    }
-
-                    Log.e("LoadMessageTask", buffer.toString());
-
-                    //Parsing JSON and displaying messages
-                    JSONObject json = new JSONObject(buffer.toString());
-                    JSONArray jmsg = json.getJSONArray("msg");
-
-                        JSONObject jmessage = jmsg.getJSONObject(0);
-                        String br = jmessage.getString("branch");
-                        String fast = jmessage.getString("Fast7");
-                        String thor = jmessage.getString("Thor");
-                        String taken = jmessage.getString("Taken");
-
-                        Map<String, String> item = new HashMap<String, String>();
-                        item.put("Branch", br);
-                        item.put("Fast7", fast);
-                        item.put("Thor", thor);
-                        item.put("Taken", taken);
-                        data.add(0, item);
-                        System.out.println("Hellooooooooooooooo");
-
-
-                    return true;
-
-                }
-            } catch (MalformedURLException e) {
-                Log.e("LoadMessageTask", "Invalid URL");
-            } catch (IOException e) {
-                Log.e("LoadMessageTask", "I/O Exception");
-            } catch (JSONException e) {
-                Log.e("LoadMessageTask", "Invalid JSON");
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
             }
-            return false;
 
-
+            return super.onOptionsItemSelected(item);
         }
 
 
+        class LoadNumberTask extends AsyncTask<String, Void, Boolean> {
+
+            @Override
+            protected Boolean doInBackground(String... params) {
+                BufferedReader reader;
+                StringBuilder buffer = new StringBuilder();
+                String line;
+
+                int fastL;
+                int thorL;
+                int takenL;
+
+
+                try {
+                    URL u = new URL("http://ict.siit.tu.ac.th/~u5522773787/its333/fetch.php");
+                    HttpURLConnection h = (HttpURLConnection) u.openConnection();
+                    h.setDoInput(true);
+                    h.connect();
+
+                    int response = h.getResponseCode();
+                    if (response == 200) {
+                        reader = new BufferedReader(new InputStreamReader(h.getInputStream()));
+                        while ((line = reader.readLine()) != null) {
+                            buffer.append(line);
+                        }
+
+//                   Log.e("LoadNumberTask", buffer.toString());
+
+                        //Parsing JSON and displaying messages
+                        JSONObject json = new JSONObject(buffer.toString());
+                        JSONArray jmsg = json.getJSONArray("msg");
+
+
+                        JSONObject jmessage = jmsg.getJSONObject(0);
+
+                        fastL = jmessage.getInt("Fast7");
+                        thorL = jmessage.getInt("Thor");
+                        takenL = jmessage.getInt("Taken");
+
+
+                        HashMap<String, String> item;
+                        item = new HashMap<String, String>();
+
+                        if (fastL >= thorL && thorL >= takenL) {
+                            item.put("Fast7", String.format("Fast7  " + "%d", fastL));
+                            item.put("Thor", String.format("Thor  " + "%d", thorL));
+                            item.put("Taken", String.format("Taken  " + "%d", takenL));
+                            data.add(0, item);
+                        }
+                        if (fastL >= thorL && thorL <= takenL) {
+                            item.put("Fast7", String.format("Fast7  " + "%d", fastL));
+                            item.put("Thor", String.format("Thor  " + "%d", takenL));
+                            item.put("Taken", String.format("Taken  " + "%d", thorL));
+                            data.add(0, item);
+                        }
+                        if (fastL <= thorL && fastL >= takenL) {
+                            item.put("Fast7", String.format("Thor  " + "%d", thorL));
+                            item.put("Thor", String.format("Fast7  " + "%d", fastL));
+                            item.put("Taken", String.format("Taken  " + "%d", takenL));
+                            data.add(0, item);
+                        }
+                        if (fastL <= thorL && fastL <= takenL) {
+                            item.put("Fast7", String.format("Thor  " + "%d", thorL));
+                            item.put("Thor", String.format("Taken  " + "%d", takenL));
+                            item.put("Taken", String.format("Fast7  " + "%d", fastL));
+                            data.add(0, item);
+                        }
+                        if (fastL >= thorL && fastL <= takenL) {
+                            item.put("Fast7", String.format("Taken  " + "%d", takenL));
+                            item.put("Thor", String.format("Fast7  " + "%d", fastL));
+                            item.put("Taken", String.format("Thor  " + "%d", thorL));
+                            data.add(0, item);
+                        }
+                        if (fastL <= thorL && fastL <= takenL) {
+                            item.put("Fast7", String.format("Taken  " + "%d", takenL));
+                            item.put("Thor", String.format("Thor  " + "%d", thorL));
+                            item.put("Taken", String.format("Fast7  " + "%d", fastL));
+                            data.add(0, item);
+                        }
+                    }
+
+                    return true;
+
+
+                } catch (MalformedURLException e) {
+                    Log.e("LoadNumberTask", "Invalid URL");
+                } catch (IOException e) {
+                    Log.e("LoadNumberTask", "I/O Exception");
+                } catch (JSONException e) {
+                    Log.e("LoadNumberTask", "Invalid JSON");
+                }
+                return false;
+            }
+
+            protected void onPostExecute(Boolean result) {
+                if (result) {
+                    Log.e("LoadNumberTask", "Successfully Load");
+                } else {
+                    Log.e("LoadNumberTask", "Unable Load");
+
+                }
+            }
+        }
     }
-}
->>>>>>> origin/master
+
+
+
